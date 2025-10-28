ea1d5164c27bc9e1e9f4165d46ba2b5c7d0349d5
RaulSanM <r.sanmartinyhendrikx@student.fontys.nl>
2025-10-28 11:23:10 +0100

    Added exports for user repo and user model. But Rene needs to push it into dev so that I can use this. Also created repo's for all models

--- backend/backend/src/models/index.ts
+++ backend/backend/src/models/index.ts
@@ -2,3 +2,4 @@ export * from './badge.model';
 export * from './user-badge.model';
 export * from './exploration-event.model';
 export * from './user-streak.model';
+export * from './user.model';
---
+++ backend/backend/src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './badge.repository';
+export * from './user-badge.repository';
+export * from './exploration-event.repository';
+export * from './user-streak.repository';
+export * from './user.repository';
