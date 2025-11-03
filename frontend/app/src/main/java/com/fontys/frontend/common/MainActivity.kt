9fcf890f5a911a8710b67dfb947e02c850028ea2
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-11-03 13:03:01 +0100

    MainActivity

---
+++ frontend/app/src/main/java/com/fontys/frontend/common/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.fontys.frontend.common
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Text(text = "Hello World!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MaterialTheme {
+        MainScreen()
+    }
+}
