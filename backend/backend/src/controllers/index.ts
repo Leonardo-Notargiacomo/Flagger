73b4aef53a61df48914966db99497c21dbcea65d
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-11-05 10:21:10 +0100

    backend fix #2
    The functionality is in the "badge" branch
    
    For now, we don't need it here

--- backend/backend/src/controllers/index.ts
+++ backend/backend/src/controllers/index.ts
@@ -1,7 +1,4 @@
 export * from './ping.controller';
-export * from './badge.controller';
-export * from './exploration.controller';
-export * from './user.controller';
 export * from './go-user.controller';
 export * from './flag.controller';
 export * from './flag-go-user.controller';
