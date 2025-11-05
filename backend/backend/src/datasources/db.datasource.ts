5357ca19f12b4925d9e299da3119ec0914017c97
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-11-05 10:32:07 +0100

    Update db.datasource.ts

--- backend/backend/src/datasources/db.datasource.ts
+++ backend/backend/src/datasources/db.datasource.ts
@@ -4,10 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'db',
   connector: 'postgresql',
-  url:'postgresql://project3_android_001_user:G1N08w9qp1AG41A5GsShhQns3jsX2kl2@dpg-d3h7qdp5pdvs73f21ssg-a.frankfurt-postgres.render.com/project3_android_001?ssl=true',
-  ssl: {
-    rejectUnauthorized: false
-  }
+  url:'postgresql://project3_android_001_user:G1N08w9qp1AG41A5GsShhQns3jsX2kl2@dpg-d3h7qdp5pdvs73f21ssg-a.frankfurt-postgres.render.com/project3_android_001?sslmode=required',
 };
 
 // Observe application's life cycle to disconnect the datasource when
