f206b25a0f73559c7d95ea3fd9d5f6503b93f9c7
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-11-06 18:50:20 +0100

    friendRequestModel

--- backend/backend/src/models/index.ts
+++ backend/backend/src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './go-user.model';
 export * from './flag.model';
+export * from './friend-request.model';
 export * from './go-user-credentials.model';
--- backend/backend/src/repositories/index.ts
+++ backend/backend/src/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from './go-user.repository';
 export * from './flag.repository';
+export * from './friend-request.repository';
 export * from './go-user-credentials.repository';
