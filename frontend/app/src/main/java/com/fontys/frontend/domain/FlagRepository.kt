23ac3f9bd20687f7d90afe37482c1cb5ee25c8a7
RaulSanM <r.sanmartinyhendrikx@student.fontys.nl>
2025-11-10 16:31:49 +0100

    I think it works

--- frontend/app/src/main/java/com/fontys/frontend/domain/FlagRepository.kt
+++ frontend/app/src/main/java/com/fontys/frontend/domain/FlagRepository.kt
@@ -19,7 +19,7 @@ import com.fontys.frontend.data.FlagResponse // Import your new FlagResponse dat
 
 class FlagRepository{
 
-    val BASE_URL = "https://group-repository-2025-android-1.onrender.com/"
+    val BASE_URL = "https://group-repository-2025-android-1-6of2.onrender.com/"
     var token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYW1lIjoiZ2FuZ3N0YWxrZWQiLCJlbWFpbCI6Ind3d0B3dy53dyIsImlhdCI6MTc2MjQxNjk0MSwiZXhwIjoxNzYyNDM4NTQxfQ.xowNshI30rRavngwql8eIJ59NADVvpsHGXtXaPXJyFQ"
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
