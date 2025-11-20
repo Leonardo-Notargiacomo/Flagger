66ac54db5ce26b1ae6c7de5ed9ce48dd35f780ba
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-11-20 14:11:51 +0100

    merging fix

--- frontend/app/src/main/java/com/fontys/frontend/ui/views/Registration.kt
+++ frontend/app/src/main/java/com/fontys/frontend/ui/views/Registration.kt
@@ -126,17 +126,10 @@ fun RegistrationView(
                     password.length < 8 -> {
                         errorMessage = "Password must be at least 8 characters"
                     }
-<<<<<<< HEAD
-                    !password.matches("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}\$/gm".toRegex()) -> {
-                        errorMessage = "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character."
-                    }
-                    !email.matches("/(?:[a-z0-9!#\$%&'*+\\x2f=?^_`\\x7b-\\x7d~\\x2d]+(?:\\.[a-z0-9!#\$%&'*+\\x2f=?^_`\\x7b-\\x7d~\\x2d]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9\\x2d]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9\\x2d]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\\x2d]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+))/".toRegex()) -> {
-=======
                     !password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$".toRegex()) -> {
                         errorMessage = "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character."
                     }
                     !email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$".toRegex()) -> {
->>>>>>> 47a7e21ac13bf8dd0614beda393857acb146c6ef
                         errorMessage = "The email must follow a valid email format."
                     }
                     else -> {
