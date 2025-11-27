ec19d1ab85211b7af499309ca041cc0a18f73605
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-11-27 22:55:59 +0100

    feedback: text beneath navbar

--- frontend/app/src/main/java/com/fontys/frontend/ui/views/Navbar.kt
+++ frontend/app/src/main/java/com/fontys/frontend/ui/views/Navbar.kt
@@ -9,13 +9,20 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.EmojiEvents
 import androidx.compose.material.icons.filled.Map
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.fontys.frontend.common.BadgeView
 import com.fontys.frontend.common.FriendView
@@ -26,53 +33,111 @@ import com.fontys.frontend.common.ProfileView
 @Composable
 fun NavBar() {
     val navController = rememberNavController()
-    val scope = rememberCoroutineScope()
-
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         contentWindowInsets = WindowInsets(0, 0, 0, 0),
         bottomBar = {
-            NavigationBar {
-                NavigationBarItem(
-                    selected = false,
-                    onClick = {navController.navigate(MapView)},
-                    icon = { Icon(
-                        modifier = Modifier.size(40.dp),
-                        imageVector = Icons.Default.Map,
-                        contentDescription = "Map"
-                    ) }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    onClick = {navController.navigate(FriendView)},
-                    icon = { Icon(
-                        modifier = Modifier.size(40.dp),
-                        imageVector = Icons.AutoMirrored.Filled.Comment,
-                        contentDescription = "Friends"
-                    ) }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    onClick = {navController.navigate(BadgeView)},
-                    icon = { Icon(
-                        modifier = Modifier.size(40.dp),
-                        imageVector = Icons.Default.EmojiEvents,
-                        contentDescription = "Badges"
-                    ) }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    onClick = {navController.navigate(ProfileView)},
-                    icon = { Icon(
-                        modifier = Modifier.size(40.dp),
-                        imageVector = Icons.Default.AccountCircle,
-                        contentDescription = "Profile"
-                    ) }
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            ) {
+                val navItemColors = NavigationBarItemDefaults.colors(
+                    selectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    selectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    indicatorColor = MaterialTheme.colorScheme.secondaryContainer
                 )
 
+                NavigationBarItem(
+                    selected = currentDestination?.hierarchy?.any { it.hasRoute<MapView>() } == true,
+                    onClick = {
+                        navController.navigate(MapView) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Default.Map,
+                            contentDescription = "Map"
+                        )
+                    },
+                    label = { Text("Map") },
+                    colors = navItemColors
+                )
+                NavigationBarItem(
+                    selected = currentDestination?.hierarchy?.any { it.hasRoute<FriendView>() } == true,
+                    onClick = {
+                        navController.navigate(FriendView) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.AutoMirrored.Filled.Comment,
+                            contentDescription = "Friends"
+                        )
+                    },
+                    label = { Text("Friends") },
+                    colors = navItemColors
+                )
+                NavigationBarItem(
+                    selected = currentDestination?.hierarchy?.any { it.hasRoute<BadgeView>() } == true,
+                    onClick = {
+                        navController.navigate(BadgeView) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Default.EmojiEvents,
+                            contentDescription = "Badges"
+                        )
+                    },
+                    label = { Text("Badges") },
+                    colors = navItemColors
+                )
+                NavigationBarItem(
+                    selected = currentDestination?.hierarchy?.any { it.hasRoute<ProfileView>() } == true,
+                    onClick = {
+                        navController.navigate(ProfileView) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Profile"
+                        )
+                    },
+                    label = { Text("Profile") },
+                    colors = navItemColors
+                )
             }
-        }) { padding ->
+        }
+    ) { padding ->
         NavHost(navController, padding)
     }
 }
