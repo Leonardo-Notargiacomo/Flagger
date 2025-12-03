a9ea1e6e6c039a56013b2facd28431336e1485d4
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-12-03 14:13:48 +0100

    Update ExploreNotificationManager.kt

--- frontend/app/src/main/java/com/fontys/frontend/utils/ExploreNotificationManager.kt
+++ frontend/app/src/main/java/com/fontys/frontend/utils/ExploreNotificationManager.kt
@@ -59,8 +59,6 @@ class ExploreNotificationManager(private val context: Context) {
 
         // Build the notification
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(title)
             .setContentText(message)
             .setStyle(NotificationCompat.BigTextStyle()
                 .bigText(message)
