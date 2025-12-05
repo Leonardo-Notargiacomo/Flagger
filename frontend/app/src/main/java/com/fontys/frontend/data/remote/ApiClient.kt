14bb625ac72c474c43388ffe7f1f66b85ca7d71c
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-12-05 23:08:17 +0100

    Integrate FCM token registration with backend

--- frontend/app/src/main/java/com/fontys/frontend/data/remote/ApiClient.kt
+++ frontend/app/src/main/java/com/fontys/frontend/data/remote/ApiClient.kt
@@ -27,4 +27,5 @@ object ApiClient {
 
     val badgeApi: BadgeApi = retrofit.create(BadgeApi::class.java)
     val friendsApi: FriendsApi = retrofit.create(FriendsApi::class.java)
+    val notificationApi: NotificationApiService = retrofit.create(NotificationApiService::class.java)
 }
