d0f9ba01d60f1c1c369712631fa101513a9fcd19
danya <553893@student.fontys.nl>
2025-12-08 10:32:24 +0000

    dbpersonal

--- backend/backend/src/datasources/db.datasource.ts
+++ backend/backend/src/datasources/db.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'db',
   connector: 'postgresql',
-  url: 'postgresql://android_group01_user:Ojv0Yq3bSmRWbb6CAeMsGMnWGDhpJIWr@dpg-d4mmdavpm1nc73d22460-a.frankfurt-postgres.render.com/android_group01?ssl=true'
+  url: 'postgresql://gopersonal:tqqdxP1AoqM194zFZ2mMKslMCuYxKoTe@dpg-d4raghogjchc73bp4m6g-a.frankfurt-postgres.render.com/gopersonal?ssl=true'
 };
 
 // Observe application's life cycle to disconnect the datasource when
