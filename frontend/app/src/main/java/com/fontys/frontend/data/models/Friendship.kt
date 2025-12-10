ec351ac01cc06cc3dce6f33a97faec9cf0cc33a3
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-12-10 17:24:25 +0100

    delete the friendship
    Cause the FriendListItem is used

--- frontend/app/src/main/java/com/fontys/frontend/data/models/Friendship.kt
+++ frontend/app/src/main/java/com/fontys/frontend/data/models/Friendship.kt
@@ -2,24 +2,6 @@ package com.fontys.frontend.data.models
 
 import com.google.gson.annotations.SerializedName
 
-data class Friendship(
-    @SerializedName("id")
-    val id: Int?,
-
-    @SerializedName("userId")
-    val userId: Int,
-
-    @SerializedName("friendId")
-    val friendId: Int,
-
-    @SerializedName("createdAt")
-    val createdAt: String?,
-
-    // Friend details from /friends endpoint
-    @SerializedName("friendDetails")
-    val friendDetails: User? = null
-)
-
 data class FriendListItem(
     @SerializedName("id")
     val id: Int?,
