7d9349dc67d300d0ea9bae26d0699f5f2e515610
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-12-12 22:33:39 +0100

    more UX badge unlocking

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -115,6 +115,7 @@ dependencies {
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.accompanist.permissions)
+    implementation(libs.lottie.compose)
 
 }
 
